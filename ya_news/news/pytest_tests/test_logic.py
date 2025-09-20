8267e583e8ded104d121498a9211450012de3ccc
Семён <semeneroshevich@yandex.ru>
2025-09-20 10:14:10 +0700

    rework11

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -1,11 +1,12 @@
 from http import HTTPStatus
 
 import pytest
-from news.models import Comment
+
 from news.forms import BAD_WORDS, WARNING
+from news.models import Comment
 
 
-TEXT = 'Текст комментария'
+FORM_DATA = {'text': 'Текст комментария'}
 BAD_WORDS_DATA = [
     {'text': f'Какой-то текст, {bad_word}, еще текст'}
     for bad_word in BAD_WORDS
@@ -16,20 +17,20 @@ pytestmark = pytest.mark.django_db
 
 
 def test_anonym_cant_create_comment(client, detail_url):
-    client.post(detail_url, data={'text': TEXT})
+    client.post(detail_url, data=FORM_DATA)
     assert Comment.objects.count() == 0
 
 
 def test_user_can_create_comment(
         author_client, detail_url, comments_url, news, author):
-    response = author_client.post(detail_url, data={'text': TEXT})
+    response = author_client.post(detail_url, data=FORM_DATA)
     assert response.status_code == HTTPStatus.FOUND
     assert response.url == comments_url
 
     assert Comment.objects.count() == 1
 
     comment = Comment.objects.get()
-    assert comment.text == TEXT
+    assert comment.text == FORM_DATA['text']
     assert comment.news == news
     assert comment.author == author
 
@@ -44,29 +45,27 @@ def test_user_cant_use_bad_words(author_client, detail_url, bad_words_data):
 
 def test_author_can_edit_comment(
         author_client, edit_url, comments_url, comment):
-    new_text = 'Обновлённый комментарий'
-    response = author_client.post(edit_url, data={'text': new_text})
+    updated_form_data = {'text': 'Обновлённый комментарий'}
+    response = author_client.post(edit_url, data=updated_form_data)
     assert response.status_code == HTTPStatus.FOUND
     assert response.url == comments_url
 
     updated_comment = Comment.objects.get(id=comment.id)
-    assert updated_comment.text == new_text
+    assert updated_comment.text == updated_form_data['text']
     assert updated_comment.news == comment.news
     assert updated_comment.author == comment.author
-    assert updated_comment.created == comment.created
 
 
 def test_user_cant_edit_comment_of_another_user(
         reader_client, edit_url, comment):
-    new_text = 'Обновлённый комментарий'
-    response = reader_client.post(edit_url, data={'text': new_text})
+    updated_form_data = {'text': 'Обновлённый комментарий'}
+    response = reader_client.post(edit_url, data=updated_form_data)
     assert response.status_code == HTTPStatus.NOT_FOUND
 
     unchanged_comment = Comment.objects.get(id=comment.id)
     assert unchanged_comment.text == comment.text
     assert unchanged_comment.news == comment.news
     assert unchanged_comment.author == comment.author
-    assert unchanged_comment.created == comment.created
 
 
 def test_author_can_delete_comment(
--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -13,54 +13,53 @@ from .test_base import (
 class TestLogic(BaseTest):
 
     def test_author_can_create_note(self):
-        initial_notes = set(Note.objects.values_list('id', flat=True))
+        initial_notes_ids = set(Note.objects.values_list('id', flat=True))
         response = self.author_client.post(NOTE_ADD_URL, data=self.form_data)
         self.assertRedirects(response, SUCCESS_URL)
 
-        final_notes = set(Note.objects.values_list('id', flat=True))
-        new_notes = final_notes - initial_notes
-        self.assertEqual(len(new_notes), 1)
-        new_note = Note.objects.get(id=new_notes.pop())
+        final_notes_ids = set(Note.objects.values_list('id', flat=True))
+        new_notes_ids = final_notes_ids - initial_notes_ids
+        self.assertEqual(len(new_notes_ids), 1)
+        new_note = Note.objects.get(id=new_notes_ids.pop())
         self.assertEqual(new_note.title, self.form_data['title'])
         self.assertEqual(new_note.text, self.form_data['text'])
         self.assertEqual(new_note.slug, self.form_data['slug'])
         self.assertEqual(new_note.author, self.author)
 
     def test_anonymous_user_cant_create_note(self):
-        initial_notes = set(Note.objects.values_list('id', flat=True))
+        initial_notes_ids = set(Note.objects.values_list('id', flat=True))
         response = self.client.post(NOTE_ADD_URL, data=self.form_data)
         self.assertRedirects(response, REDIRECT_NOTE_ADD_URL)
         self.assertEqual(
-            set(Note.objects.values_list('id', flat=True)), initial_notes)
+            set(Note.objects.values_list('id', flat=True)), initial_notes_ids)
 
     def test_not_unique_slug(self):
         self.form_data['slug'] = self.note.slug
-        initial_notes = set(Note.objects.values_list('id', flat=True))
+        initial_notes_ids = set(Note.objects.values_list('id', flat=True))
         response = self.author_client.post(NOTE_ADD_URL, data=self.form_data)
         form = response.context['form']
         self.assertIn('slug', form.errors)
         self.assertIn(self.note.slug + WARNING, form.errors['slug'])
         self.assertEqual(
-            set(Note.objects.values_list('id', flat=True)), initial_notes)
+            set(Note.objects.values_list('id', flat=True)), initial_notes_ids)
 
     def test_empty_slug(self):
         del self.form_data['slug']
-        initial_notes = set(Note.objects.values_list('id', flat=True))
+        initial_notes_ids = set(Note.objects.values_list('id', flat=True))
         response = self.author_client.post(NOTE_ADD_URL, data=self.form_data)
         self.assertRedirects(response, SUCCESS_URL)
 
-        final_notes = set(Note.objects.values_list('id', flat=True))
-        new_notes = final_notes - initial_notes
-        self.assertEqual(len(new_notes), 1)
+        final_notes_ids = set(Note.objects.values_list('id', flat=True))
+        new_notes_ids = final_notes_ids - initial_notes_ids
+        self.assertEqual(len(new_notes_ids), 1)
 
-        new_note = Note.objects.get(id=new_notes.pop())
+        new_note = Note.objects.get(id=new_notes_ids.pop())
         self.assertEqual(new_note.slug, slugify(self.form_data['title']))
         self.assertEqual(new_note.title, self.form_data['title'])
         self.assertEqual(new_note.text, self.form_data['text'])
         self.assertEqual(new_note.author, self.author)
 
     def test_author_can_edit_note(self):
-        original_author = self.note.author
         response = self.author_client.post(EDIT_URL, data=self.form_data)
         self.assertRedirects(response, SUCCESS_URL)
 
@@ -68,7 +67,7 @@ class TestLogic(BaseTest):
         self.assertEqual(updated_note.title, self.form_data['title'])
         self.assertEqual(updated_note.text, self.form_data['text'])
         self.assertEqual(updated_note.slug, self.form_data['slug'])
-        self.assertEqual(updated_note.author, original_author)
+        self.assertEqual(updated_note.author, self.note.author)
 
     def test_not_author_cant_edit_note(self):
         response = self.not_author_client.post(
@@ -89,13 +88,12 @@ class TestLogic(BaseTest):
         self.assertFalse(Note.objects.filter(id=self.note.id).exists())
 
     def test_not_author_cant_delete_note(self):
-        initial_notes = set(Note.objects.values_list('id', flat=True))
+        initial_notes_ids = set(Note.objects.values_list('id', flat=True))
         response = self.not_author_client.post(DELETE_URL)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         self.assertEqual(
-            set(Note.objects.values_list('id', flat=True)), initial_notes)
+            set(Note.objects.values_list('id', flat=True)), initial_notes_ids)
 
-        self.assertTrue(Note.objects.filter(id=self.note.id).exists())
         existing_note = Note.objects.get(id=self.note.id)
         self.assertEqual(existing_note.title, self.note.title)
         self.assertEqual(existing_note.text, self.note.text)
